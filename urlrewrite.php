f4e1cca62e7eb3effe0bb8be3e46289841608778
Amanat <a.aristanov@ddemo.ru>
2024-02-17 14:19:11 +0500

    initial commit

---
+++ urlrewrite.php
@@ -0,0 +1,19 @@
+<?php
+$arUrlRewrite=array (
+  1 => 
+  array (
+    'CONDITION' => '#^/content/articles/#',
+    'RULE' => '',
+    'ID' => 'bitrix:news',
+    'PATH' => '/content/articles/index.php',
+    'SORT' => 100,
+  ),
+  0 => 
+  array (
+    'CONDITION' => '#^/rest/#',
+    'RULE' => '',
+    'ID' => NULL,
+    'PATH' => '/bitrix/services/rest/index.php',
+    'SORT' => 100,
+  ),
+);
